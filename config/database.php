7cd2f8a708f08479253f370895cf494dbe76187c
Greg Baugues <greg@baugues.com>
2015-02-23 15:56:13 -0600

    Update database.php to work with heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
 	/*
@@ -26,7 +33,7 @@ return [
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => 'postgres',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -67,10 +74,10 @@ return [
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host'     => $host,
+			'database' => $database,
+			'username' => $username,
+			'password' => $password,
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
